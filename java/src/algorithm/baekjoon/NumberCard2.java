4a2b597126e9704d11c7ffd4b369901f14dea88a
whco <einsteized@gmail.com>
2021-10-27 23:23:30 +0900

    baekjoon added

--- java/src/algorithm/baekjoon/NumberCard2.java
+++ java/src/algorithm/baekjoon/NumberCard2.java
@@ -1,5 +1,35 @@
 package algorithm.baekjoon;
 
-//public class NumberCard2 {
-//    int n = Scanner.
-//}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class NumberCard2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        HashMap<Integer, Integer> owningCardMap = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            owningCardMap.put(num, owningCardMap.getOrDefault(num, 0) + 1);
+        }
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+//        int[] requiredCard = new int[m];
+        st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(owningCardMap.getOrDefault(num, 0) + " ");
+        }
+        System.out.println(sb);
+
+    }
+
+
+}
