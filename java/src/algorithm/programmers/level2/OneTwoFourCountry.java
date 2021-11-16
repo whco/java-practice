5b120406412f2a61e3aa35ece1ffa7f76bad9708
whco <einsteized@gmail.com>
2021-11-16 16:55:24 +0900

    ADD OneTwoFourCountry.java

---
+++ java/src/algorithm/programmers/level2/OneTwoFourCountry.java
@@ -0,0 +1,13 @@
+package algorithm.programmers.level2;
+
+class Solution {
+    public String solution(int n) {
+        char[] oneTwoFour = {'1', '2', '4'};
+        StringBuffer sb = new StringBuffer();
+        while(n != 0){
+            sb.append(oneTwoFour[(n - 1) % 3]);
+            n = (n - 1) / 3;
+        }
+        return sb.reverse().toString();
+    }
+}
