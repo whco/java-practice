2436113a814f00473baff028a8ed620b312cabd3
whco <einsteized@gmail.com>
2021-12-26 23:39:03 +0900

    ADD java basics HashSetDeepCopyShallowCopy

---
+++ java/src/algorithm/baekjoon/Yaminjeonguem.java
@@ -0,0 +1,25 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Yaminjeonguem {
+    private final int MAX = (int)1e9;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        boolean[] visited = new boolean[n];
+        int[] dist = new int[n];
+
+
+    }
+}
